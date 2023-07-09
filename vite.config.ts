65e61f85c2a718a013a4d0121f602412a42d69b9
杜晓浩 <1923911906@qq.com>
2023-07-09 15:59:12 +0800

    🐞 fix: 解决打包问题

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,20 @@ import path from 'path';
 
 export default defineConfig({
 	plugins: [],
-	server: { host: '0.0.0.0', port: 8000 },
 	clearScreen: false,
+	base: './',
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "src"),
 		}
 	},
+	server: {
+		host: '0.0.0.0',
+		port: 8000,
+		open: true,
+	},
 	build: {
+		assetsInlineLimit: 0,
 		chunkSizeWarningLimit: 1500,
 	}
 });
